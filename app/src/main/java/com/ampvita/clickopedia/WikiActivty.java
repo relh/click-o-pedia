31c789360f86556e65844ad4e32329ee4aa5bacb
relh <richard@relh.net>
2015-01-18 05:38:41 -0500

    this app is easy

--- app/src/main/java/com/ampvita/clickopedia/WikiActivty.java
+++ app/src/main/java/com/ampvita/clickopedia/WikiActivty.java
@@ -1,8 +1,6 @@
 package com.ampvita.clickopedia;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -10,16 +8,10 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.Random;
 
 
 public class WikiActivty extends Activity implements View.OnTouchListener, Handler.Callback {
@@ -29,22 +21,23 @@ public class WikiActivty extends Activity implements View.OnTouchListener, Handl
 
     private final Handler handler = new Handler(this);
 
+    private TextView scoreView;
+    private int score;
+
     private WebView webView;
     private WebViewClient client;
 
-    SharedPreferences prefs;
     String lastUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_wiki_activty);
-        webView = (WebView) findViewById(R.id.wiki_view);
-        webView.setOnTouchListener(this);
 
-        prefs = this.getSharedPreferences(
-                "com.ampvita.clickopedia", Context.MODE_PRIVATE);
+        score = -1;
+        scoreView = (TextView) findViewById(R.id.score_view);
+        webView = (WebView) findViewById(R.id.web_view);
+        webView.setOnTouchListener(this);
 
         client = new WebViewClient(){
             @Override public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -56,18 +49,16 @@ public class WikiActivty extends Activity implements View.OnTouchListener, Handl
 
         webView.setWebViewClient(client);
         webView.setVerticalScrollBarEnabled(false);
-        webView.loadUrl("http://www.wikipedia.org");
 
-        HashSet<String> hardCoded = (HashSet) prefs.getStringSet("top5000", new HashSet<String>());
-
-        System.out.println(hardCoded.toString());
-
-        //String[] arr = lines.toArray(new String[0]);
+        //start you on a random page
+        int idx = new Random().nextInt(ClickopediaApplication.top5000.length);
+        String random = (ClickopediaApplication.top5000[idx]);
+        webView.loadUrl("http://www.en.wikipedia.org/wiki" + random);
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        if (v.getId() == R.id.wiki_view && event.getAction() == MotionEvent.ACTION_DOWN){
+        if (v.getId() == R.id.web_view && event.getAction() == MotionEvent.ACTION_DOWN){
             handler.sendEmptyMessageDelayed(CLICK_ON_WEBVIEW, 500);
         }
         return false;
@@ -75,18 +66,35 @@ public class WikiActivty extends Activity implements View.OnTouchListener, Handl
 
     @Override
     public boolean handleMessage(Message msg) {
-        if (msg.what == CLICK_ON_URL){
-            int clicks = prefs.getInt("clicks", 0); // add one to your clicks counter
-            prefs.edit().putInt("clicks", clicks+1);
-            handler.removeMessages(CLICK_ON_WEBVIEW);
-            Toast.makeText(this, "Url " + lastUrl, Toast.LENGTH_SHORT).show();
-            webView.loadUrl(lastUrl);
+        if (msg.what == CLICK_ON_URL) {
+            handler.removeMessages(CLICK_ON_WEBVIEW); //remove normal screen clicks
+
+            Toast.makeText(this, "Url " + lastUrl, Toast.LENGTH_SHORT).show(); //display url
+
+            //check for webpage being a wikipage
+            if (lastUrl.toLowerCase().contains("wikipedia")) {
+                score++;
+                webView.loadUrl(lastUrl); //load webpage
+            }
+
+            scoreView.setText("Clicks so far: " + score); //update number of clicks
             return true;
         }
-        if (msg.what == CLICK_ON_WEBVIEW){
-            Toast.makeText(this, "WebView clicked", Toast.LENGTH_SHORT).show();
+        if (msg.what == CLICK_ON_WEBVIEW) {
+            //Toast.makeText(this, "WebView clicked", Toast.LENGTH_SHORT).show();
             return true;
         }
         return false;
     }
 }
+
+//SharedPreferences prefs;
+
+//prefs = this.getSharedPreferences(
+//        "com.ampvita.clickopedia", Context.MODE_PRIVATE);
+
+//int clicks = prefs.getInt("clicks", 0) + 1; // add one to your clicks score
+//prefs.edit().putInt("clicks", clicks);
+
+//HashSet<String> hardCoded = (HashSet) prefs.getStringSet("top5000", new HashSet<String>());
+//System.out.println(hardCoded.toString());
