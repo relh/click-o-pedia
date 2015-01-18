cc325f97acf0f0810b361b49374d7b50cdcf76c4
relh <richard@relh.net>
2015-01-18 03:16:23 -0500

    got top 5000 and some scraping

--- scrape.py
+++ scrape.py
@@ -1,15 +1,21 @@
 import urllib2
+import re
+
 response = urllib2.urlopen('http://en.wikipedia.org/wiki/User:West.andrew.g/Popular_pages')
 html = response.read()
-print html
+# print html
 
-import re
 i = 1
-while i < 5:#5000:
-	a = re.compile(str(i)+'</td>\\n<td><a href=\"/wiki/\w+')#\"")
-	print a.search(html).group(0)
-	print i
+f = open("./top5000", 'w')
+while i < 5000:
+	lazyItem = re.compile(str(i)+'</td>\\n<td><a href=\"/wiki/[^"]+')#\"")
+	search = lazyItem.search(html)
+	if search:
+		search = search.group(0)
+		actualTitle = search.split("wiki",1)[-1] 
+		print actualTitle
+		f.write(actualTitle+"\n")
+		print i
 	i += 1
-
 
 	#<td>"+str(i)+"</td><td><a href=\"
