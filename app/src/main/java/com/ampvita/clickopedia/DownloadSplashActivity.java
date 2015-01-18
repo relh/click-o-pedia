7d377014fed2ac81ebb9b3a2e09a6ffaf9cb47f3
relh <richard@relh.net>
2015-01-18 09:12:16 -0500

    fixed synchronous stuff

--- app/src/main/java/com/ampvita/clickopedia/DownloadSplashActivity.java
+++ app/src/main/java/com/ampvita/clickopedia/DownloadSplashActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.widget.ImageView;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
@@ -19,16 +20,76 @@ public class DownloadSplashActivity extends Activity {
     Firebase mfr;
     ImageView splash;
 
-    boolean ownValues = false;
+    boolean firstStart = true;
+    boolean firstFinish = true;
 
-    boolean gotStart = false;
-    boolean gotFinish = false;
-
-    boolean deletedStart = false;
-    boolean deletedFinish = false;
+    int openGame = 0;
 
     String start;
     String finish;
+
+    Random r;
+
+    ValueEventListener FirebaseListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot snapshot) {
+            System.out.println(snapshot.getKey());
+            System.out.println(snapshot.getValue());
+
+            if (snapshot.getKey().equals("start")) {
+                if (firstStart) { // first value
+                    firstStart = false;
+                    start = snapshot.getValue().toString();
+                    if (start != null) { // chance for open game to exist
+                        openGame++;
+                        onOpenGameChange();
+                        mfr.child("start").removeEventListener(this);
+                    } else {
+                        int idx = r.nextInt(ClickopediaApplication.top5000.length); // if starting null value, we will host
+                        start = (ClickopediaApplication.top5000[idx]);
+                    }
+                } else { // been here before, now waiting for other player
+                    start = snapshot.getValue().toString();
+                    if (start != null) { // we found a game!
+                        openGame++;
+                        onOpenGameChange();
+                        mfr.child("start").removeEventListener(this);
+                    }
+                }
+            } else { // is finish
+                if (firstFinish) { // first value
+                    firstFinish = false;
+                    finish = snapshot.getValue().toString();
+                    if (finish != null) { // chance for open game to exist
+                        openGame++;
+                        onOpenGameChange();
+                        mfr.child("finish").removeEventListener(this);
+                    } else {
+                        int idx = r.nextInt(ClickopediaApplication.top5000.length); // if starting null value, we will host
+                        finish = (ClickopediaApplication.top5000[idx]);
+                    }
+                } else { // been here before, now waiting for other player
+                    finish = snapshot.getValue().toString();
+                    if (finish != null) { // we found a game!
+                        openGame++;
+                        onOpenGameChange();
+                        mfr.child("finish").removeEventListener(this);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onCancelled(FirebaseError error) {
+        }
+    };
+
+    protected void onOpenGameChange() {
+        System.out.println(openGame);
+        if (openGame == 2) { // Got both a start and finish ideally
+            progress();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,90 +98,12 @@ public class DownloadSplashActivity extends Activity {
         splash = (ImageView) findViewById(R.id.splash);
         splash.setImageResource(R.drawable.wikipedia_splash);
 
+        r = new Random();
+
         mfr = ((ClickopediaApplication) getApplication()).myFirebaseRef;
 
-        mfr.child("start").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                System.out.println(snapshot.getValue());
-                start = snapshot.getValue().toString();
-                if (start == null) {
-                    deletedStart = true;
-                } else {
-                    mfr.child("start").removeValue();
-                }
-                gotStart = true;
-            }
-
-            @Override
-            public void onCancelled(FirebaseError error) {
-            }
-        });
-
-        mfr.child("finish").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                System.out.println(snapshot.getValue());
-                finish = snapshot.getValue().toString();
-                if (finish == null) {
-                    deletedFinish = true;
-                } else {
-                    mfr.child("finish").removeValue();
-                }
-                gotFinish = true;
-            }
-            @Override public void onCancelled(FirebaseError error) { }
-        });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // Wait for firebase read
-        while (!(gotStart && gotFinish)) {
-            try {
-                System.out.println("waiting for read");
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (start == null || finish == null) { // Gotta make our own values
-            Random r = new Random();
-
-            int idx = r.nextInt(ClickopediaApplication.top5000.length);
-            start = (ClickopediaApplication.top5000[idx]);
-
-            idx = r.nextInt(ClickopediaApplication.top5000.length);
-            finish = (ClickopediaApplication.top5000[idx]);
-
-            System.out.println("I didn't exist :(");
-
-            ownValues = true; // We're using our own key/val
-
-            mfr.child("start").setValue(start);
-            mfr.child("finish").setValue(finish);
-
-            waiting();
-        } else { // Values already existed!
-            progress();
-        }
-    }
-
-    protected void waiting() {
-        // Wait for firebase deletion by partner
-        while (!(deletedStart && deletedFinish)) {
-            try {
-                System.out.println("waiting for delete");
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        progress();
+        mfr.child("start").addValueEventListener(FirebaseListener);
+        mfr.child("finish").addValueEventListener(FirebaseListener);
     }
 
     protected void progress() {
